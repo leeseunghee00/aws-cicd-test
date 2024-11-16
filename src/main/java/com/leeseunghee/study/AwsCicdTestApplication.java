006c3489c7e5754c5765802e09a1b025ced79b0a
leeseunghee00 <lsh9246@gmail.com>
2024-11-16 23:28:28 +0900

    initialize

---
+++ src/main/java/com/leeseunghee/study/AwsCicdTestApplication.java
@@ -0,0 +1,13 @@
+package com.leeseunghee.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCicdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCicdTestApplication.class, args);
+	}
+
+}
