067fe9b5c3bd4be2c201999e934f020477bc792c
leeseunghee00 <lsh9246@gmail.com>
2024-11-17 02:12:30 +0900

    feat: Add HelloController

---
+++ src/main/java/com/leeseunghee/study/HelloController.java
@@ -0,0 +1,15 @@
+package com.leeseunghee.study;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		System.out.println("요청 성공!");
+
+		return "Hello";
+	}
+}
